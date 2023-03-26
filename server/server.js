2ea1c1640f579198ed568b472fa4548950620abd
mahfoozm <mohammadmahfoozpersonal@gmail.com>
2023-03-26 14:08:07 -0400

    initial commit

---
+++ server/server.js
@@ -0,0 +1,75 @@
+require("https").globalAgent.options.ca = require("ssl-root-cas").create();
+const express = require("express");
+const app = express();
+const sql = require("mssql");
+
+const dbConfig = {
+  user: "hidden",
+  password: "hidden",
+  server: "hidden",
+  database: "hidden",
+  options: {
+    trustServerCertificate: true, // change to false for production use-cases
+  },
+};
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.get("/api/myendpoint", async (req, res) => {
+  try {
+    const pool = new sql.ConnectionPool(dbConfig);
+    await pool.connect();
+    const result = await pool.request().query("SELECT * FROM myTable");
+    const data = result.recordset;
+    console.log("Data fetched successfully:", data);
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error fetching data from database!");
+  }
+});
+
+app.post("/api/myendpoint", async (req, res) => {
+  console.log("Received data:", req.body);
+  const data = req.body.data;
+  const pool = new sql.ConnectionPool(dbConfig);
+  await pool.connect();
+
+  try {
+    for (const row of data) {
+      console.log("Inserting row:", row);
+      await pool
+        .request()
+        .input("column1", sql.VarChar, row.column1)
+        .input("column2", sql.VarChar, row.column2)
+        .input("column3", sql.VarChar, row.column3)
+        .query(
+          "INSERT INTO myTable (column1, column2, column3) VALUES (@column1, @column2, @column3)"
+        );
+    }
+    console.log("Data inserted successfully!");
+    res.send("Data inserted successfully!");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error inserting data into database!");
+  }
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,25 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useTable } from "react-table";
+import "./Table.css";
 
-function App() {
+function Table() {
+  const [data, setData] = useState([]);
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Column 1",
+        accessor: "column1",
+      },
+      {
+        Header: "Column 2",
+        accessor: "column2",
+      },
+      {
+        Header: "Column 3",
+        accessor: "column3",
+      },
+    ],
+    []
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios.get("http://localhost:3001/api/myendpoint");
+        setData(result.data);
+      } catch (error) {
+        console.error(error);
+        alert("Error fetching data from server!");
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handleSubmit = async () => {
+    console.log("Sending data to server:", data);
+    try {
+      const updatedData = data.map((row) => {
+        return {
+          column1: row.column1,
+          column2: row.column2,
+          column3: row.column3,
+        };
+      });
+      await axios.post("http://localhost:3001/api/myendpoint", {
+        data: updatedData,
+      });
+      alert("Data successfully sent to server!");
+    } catch (error) {
+      alert("Error sending data to server!");
+      console.error(error);
+    }
+  };
+
+  const handleInputChange = (event, rowIndex, columnId) => {
+    const newData = [...data];
+    newData[rowIndex][columnId] = event.target.value;
+    setData(newData);
+  };
+
+  const handleAddRow = () => {
+    setData([...data, { column1: "", column2: "", column3: "" }]);
+  };
+
+  const handleClearTable = () => {
+    setData([{ column1: "", column2: "", column3: "" }]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="table-container">
+        <table {...getTableProps()}>
+          {/* header */}
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps()}>
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          {/* table body */}
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row, i) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell, j) => {
+                    return (
+                      <td key={`${i}-${j}`}>
+                        <input
+                          type="text"
+                          value={cell.value}
+                          onChange={(e) =>
+                            handleInputChange(e, i, cell.column.id)
+                          }
+                        />
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      <div className="logo-container">
+        <img src="/logo192.png" alt="Your logo" className="logo" />
+      </div>
+      <div className="buttons-container" style={{ clear: "both" }}>
+        <button onClick={handleAddRow}>Add Row</button>
+        <button onClick={handleClearTable}>Clear Table</button>
+        <button onClick={handleSubmit}>Send Data</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Table;
